8052dcc19882a302e3992ac36b3d2f657cd7145f
Jackymancs4 <jackyman_cs4@live.it>
2018-05-23 14:50:26 +0200

    style: lint tests with standards

--- test/attention.test.js
+++ test/attention.test.js
@@ -1,16 +1,12 @@
-'use strict';
-var expect = require('chai').expect;
-var proxyquire = require('proxyquire');
-var attention = require('../tasks/attention');
+'use strict'
+var expect = require('chai').expect
+var proxyquire = require('proxyquire')
+var attention = require('../tasks/attention')
 
-describe('grunt-attention', function(){
-
-    describe('e2e', function(){
-
-        xit('todo...', function(done){
-
-            done();
-        });
-    });
-});
-
+describe('grunt-attention', function () {
+  describe('e2e', function () {
+    xit('todo...', function (done) {
+      done()
+    })
+  })
+})
